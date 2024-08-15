fb85b6ced4ad9fd0208055ab0a25809fa671d578
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-08-15 23:18:06 +0300

    Added butons for postform

--- components/PostForm.js
+++ components/PostForm.js
@@ -3,15 +3,16 @@
 import React, { useState } from 'react';
 import css from '@/styles/postForm.module.css';
 import Block from '@/components/Block/Block';
-import { Avatar, Flex, Input } from 'antd';
+import { Avatar, Flex, Input, Button, Typography, Tooltip } from 'antd';
 import { useUser } from '@clerk/nextjs';
+import { Icon } from '@iconify/react';
 
 const PostForm = () => {
   const { user } = useUser();
   const [post, setPost] = useState('');
   return (
     <div className={css.wrapper}>
-      <Block className={css.postForm_container}>
+      <Block className={css.post_form_container}>
         <Flex style={{ width: '100%' }} gap={'1rem'}>
           <Avatar
             src={user?.imageUrl}
@@ -27,6 +28,41 @@ const PostForm = () => {
             value={post}
             onChange={(e) => setPost(e.target.value)}
           ></Input.TextArea>
+        </Flex>
+        <Flex
+          className={css.post_form_btn}
+          align='center'
+          justify='space-between'
+        >
+          {' '}
+          <Button
+            className={css.btn_image_video}
+            type='text'
+            // style={{ background: 'var(--primary)' }}
+          >
+            <Flex>
+              <Tooltip title='Image'>
+                <Icon
+                  icon={'material-symbols:image-outline'}
+                  width={'1.5rem'}
+                  style={{ marginRight: '0.5rem' }}
+                ></Icon>
+              </Tooltip>
+              <Tooltip title='Video'>
+                <Icon icon={'mingcute:video-line'} width={'1.5rem'}></Icon>
+              </Tooltip>
+            </Flex>
+          </Button>
+          <Button
+            type='primary'
+            className={css.btn_share}
+            style={{
+              background: 'var(--color-btn)',
+              color: 'black',
+            }}
+          >
+            Share
+          </Button>
         </Flex>
       </Block>
     </div>
