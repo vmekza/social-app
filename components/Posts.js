6e31d9526117725063c1aacfcc66dc5811b19e17
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-08-22 15:12:26 +0300

    Modified the Posts.js file

--- components/Posts.js
+++ components/Posts.js
@@ -1,20 +1,32 @@
 'use client';
 
 import { useInfiniteQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { getMyFeedPosts } from '@/actions/post';
 import { Flex, Spin, Typography } from 'antd';
 import { useInView } from 'react-intersection-observer';
 
 const Posts = () => {
   const { ref, inView } = useInView();
-  const { data, isLoading, isError, isSuccess } = useInfiniteQuery({
-    queryKey: 'posts',
-    queryFn: ({ pageParam = '' }) => getMyFeedPosts(pageParam),
-    getNextPageParam: (lastPage) => {
-      return lastPage?.metaData?.lastCursor;
-    },
-  });
+  const checkLastViewRef = (index, page) => {
+    if (index === page?.data?.length - 1) {
+      return true;
+    } else return false;
+  };
+  const { data, isLoading, isError, isSuccess, hasNextPage, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: 'posts',
+      queryFn: ({ pageParam = '' }) => getMyFeedPosts(pageParam),
+      getNextPageParam: (lastPage) => {
+        return lastPage?.metaData?.lastCursor;
+      },
+    });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      console.log('fetching next page');
+    }
+  }, [hasNextPage, inView, fetchNextPage]);
 
   if (isError) {
     return <Typography>Oh no, something went wrong...</Typography>;
@@ -31,19 +43,34 @@ const Posts = () => {
     return (
       <Flex vertical gap={'1rem'}>
         {data?.pages?.map((page) =>
-          page?.data?.map((post, index) => (
-            <div
-              key={index}
-              style={{
-                width: '100%',
-                background: 'blue',
-                height: '30rem',
-                borderRadius: '0.5rem',
-              }}
-            >
-              <span>Post</span>
-            </div>
-          ))
+          page?.data?.map((post, index) =>
+            checkLastViewRef(index, page) ? (
+              <div
+                key={post?.id}
+                style={{
+                  width: '100%',
+                  background: 'blue',
+                  height: '30rem',
+                  borderRadius: '0.5rem',
+                }}
+                ref={ref}
+              >
+                <span>Post</span>
+              </div>
+            ) : (
+              <div
+                key={post?.id}
+                style={{
+                  width: '100%',
+                  background: 'blue',
+                  height: '30rem',
+                  borderRadius: '0.5rem',
+                }}
+              >
+                <span>Post</span>
+              </div>
+            )
+          )
         )}
       </Flex>
     );
