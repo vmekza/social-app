c81cc2b5aadc40ce693fdc876289393844733908
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-08-22 22:56:04 +0300

    Modified Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import css from '@/styles/post.module.css';
-import { Avatar, Flex } from 'antd';
+import { Avatar, Flex, Typography } from 'antd';
 import Block from './Block/Block';
 
 const Post = ({ data }) => {
@@ -11,6 +11,12 @@ const Post = ({ data }) => {
           <Flex align='center' justify='space-between'>
             <Flex gap={'0.5rem'} align='center'>
               <Avatar size={40} src={data?.author?.image_url} />
+              <Flex vertical>
+                <Typography className='subtitle2'>
+                  {data?.author?.first_name} {''}
+                  {data?.author?.last_name}
+                </Typography>
+              </Flex>
             </Flex>
           </Flex>
         </div>
