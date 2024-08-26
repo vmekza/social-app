347e5c19c3c62b6cadcad497bdd993aec16de5a5
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-08-27 00:36:25 +0300

    Adde input area for comments

--- components/CommentInput.js
+++ components/CommentInput.js
@@ -1,10 +1,13 @@
-import { Flex } from 'antd';
+import { Flex, Avatar, Input } from 'antd';
 import React from 'react';
+import { useUser } from '@clerk/nextjs';
 
 const CommentInput = ({ postId, setOpenComments, queryId }) => {
+  const { user } = useUser();
   return (
     <Flex gap={'1rem'} align='center'>
       <Avatar src={user?.imageUrl} size={40} style={{ minWidth: '40px' }} />
+      <Input.TextArea placeholder='Spot a comment' style={{ resize: 'none' }} />
     </Flex>
   );
 };
