0fcb3eaeba3d09427fa0148bd8c4563341ce274d
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-09-03 23:35:04 +0300

    Added styles for the profile page

--- components/ProfileTop.js
+++ components/ProfileTop.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import css from '@/styles/profileTop.module.css';
+import { Image } from 'antd';
 
-const ProfileTop = () => {
+const ProfileTop = (userId, data, isLoading, isError) => {
   return (
-    <div>
-      <h1>Profile Top</h1>
+    <div className={css.profile_top_container}>
+      <div className={css.banner}>
+        <Image
+          src='/images/gym.jpg'
+          alt='banner'
+          width={'100%'}
+          height={'15rem'}
+        />
+      </div>
     </div>
   );
 };
--- sections/ProfileData/data/ProfileData.js
+++ sections/ProfileData/data/ProfileData.js
@@ -4,6 +4,7 @@ import React from 'react';
 import css from '@/styles/profileData.module.css';
 import ProfileTop from '@/components/ProfileTop';
 import { getUser } from '@/actions/user';
+import { useQuery } from '@tanstack/react-query';
 
 const ProfileData = ({ userId }) => {
   const { data, isLoading, isError } = useQuery({
@@ -14,7 +15,12 @@ const ProfileData = ({ userId }) => {
   return (
     <div className={css.wrapper}>
       <div className={css.profile_data_container}>
-        <ProfileTop />
+        <ProfileTop
+          userId={userId}
+          data={data}
+          isLoading={isLoading}
+          isError={isError}
+        />
       </div>
     </div>
   );
