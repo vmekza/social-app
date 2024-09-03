7882a950f4635cfb742fbc91e08d6887287a7e93
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-09-03 23:53:09 +0300

    Changed banner visibility

--- components/ProfileTop.js
+++ components/ProfileTop.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from '@/styles/profileTop.module.css';
 import { Image } from 'antd';
 
 const ProfileTop = (userId, data, isLoading, isError) => {
+  const [bannerView, setBannerView] = useState(false);
   return (
     <div className={css.profile_top_container}>
-      <div className={css.banner}>
+      <div className={css.banner} onClick={() => setBannerView(true)}>
         <Image
           src='/images/gym.jpg'
           alt='banner'
           width={'100%'}
           height={'15rem'}
+          preview={{
+            mask: null,
+            visible: bannerView,
+            onVisibleChange: (visible) => setBannerView(visible),
+          }}
         />
       </div>
     </div>
