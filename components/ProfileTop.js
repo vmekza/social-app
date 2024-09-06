5a1af9c7f9882035213c73f5be3fccdf5ce7465c
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-09-06 23:44:09 +0300

    Modified ProfileTop.js and profileData.module.css

--- components/ProfileTop.js
+++ components/ProfileTop.js
@@ -4,17 +4,39 @@ import css from '@/styles/profileTop.module.css';
 import { Image, Button } from 'antd';
 import { useUser } from '@clerk/nextjs';
 import { Icon } from '@iconify/react';
+import toast from 'react-hot-toast';
+import { useMutation } from '@tanstack/react-query';
 
 const ProfileTop = ({ userId, data, isLoading, isError }) => {
   const [bannerView, setBannerView] = useState(false);
   const { user } = useUser();
   const inputRef = useRef(null);
   const [banner, setBanner] = useState(null);
+
+  const handleBannerUpdate = async (e) => {
+    const file = e.target.files[0];
+    if (file && file.size > 5 * 1024 * 1024) {
+      toast.error('File size must be less than 5MB');
+      return;
+    }
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setBanner(reader.result);
+        mutate({
+          id: user?.id,
+          banner: reader.result,
+          prevBannerId: data?.data?.banner_id,
+        });
+      };
+    }
+  };
   return (
     <div className={css.profile_top_container}>
       <div className={css.banner} onClick={() => setBannerView(true)}>
         <Image
-          src='/images/gym.jpg'
+          src={banner || '/images/gym.jpg'}
           alt='banner'
           width={'100%'}
           height={'15rem'}
@@ -35,7 +57,7 @@ const ProfileTop = ({ userId, data, isLoading, isError }) => {
               accept='image/*'
               multiple={false}
               ref={inputRef}
-              onChange={(e) => handleBannerChange(e)}
+              onChange={(e) => handleBannerUpdate(e)}
               type='file'
               hidden
             />
